9097c2eecf4588e7d14564eac87861a6c13e175a
thomasgzl <thomasgonzalez.pro@gmail.com>
2021-04-23 14:41:53 +0200

    add help & random function + file-system reader package

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
 const Discord = require('discord.js');
+const fs = require('fs'); // file-system reader
 const client = new Discord.Client();
-const TOKEN = `${process.env.DJS_TOKEN}`;
+const TOKEN = `${process.env.DJS_TOKEN}`; // private token on Heroku
 
 client.once('ready', () => {
-  console.log('Ready!');
+  console.log('Ready to dance mofo!');
 });
 
 client.login(TOKEN);
 
 client.on('message', onMessage);
 
+const soundsList = fs.readdirSync('./audio');
+
 async function onMessage (message) {
   console.log(message.content);
 
+  // play a user's requested sound 
   if (message.member.voice.channel && message.content) {
     const usrMsg = message.content.replace(';', '');
     const connection = await message.member.voice.channel.join();
@@ -33,8 +37,42 @@ async function onMessage (message) {
       connection.disconnect();
     });
   }
-}
 
-/* TODO
-help : list des sons 
-random : random un son */
+  // send a private message with list of sounds available
+  if (message.member.voice.channel && message.content === ';help') {
+    const user = message.author
+    const list = []
+
+    soundsList.forEach((sound) => {
+      list.push(sound.replace('.mp3', ''))
+    });
+
+    list.forEach((element) => {
+      user.send(`;${element}`);
+    });
+  }
+
+  // randomly play a sound
+  if (message.member.voice.channel && message.content === ';rdm') {
+    const connection = await message.member.voice.channel.join();
+
+    const randomSoundIndex = Math.floor(Math.random()*soundsList.length);
+    const randomSound = soundsList[randomSoundIndex]
+
+    const dispatcher = connection.play(`audio/${randomSound}`);
+
+    dispatcher.on('start', () => {
+      console.log(`${randomSound} is randomly playing!`);
+      message.delete();
+    });
+
+    dispatcher.on('finish', () => {
+      console.log(`${randomSound} has finished playing randomly!`);
+    });
+
+    dispatcher.on('error', (err) => {
+      console.log(`${randomSound} error`, err);
+      connection.disconnect();
+    });
+  }
+}
