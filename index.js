62495fa4098c5c328d787ab6b15ad81ef62f7a7d
thomasgzl <thomasgonzalez.pro@gmail.com>
2021-08-29 15:04:54 +0200

    fix connection

--- index.js
+++ index.js
@@ -17,61 +17,61 @@ async function onMessage (message) {
   console.log(`👉🏼 ${message.content}`);
 
   // play a user's requested sound 
-  if (message.member.voice.channel && message.content.startsWith(';')) {
-    const usrMsg = message.content.replace(';', '');
-    const connection = await message.member.voice.channel.join();
+    if (message.member.voice.channel && message.content.startsWith(';')) {
+      const usrMsg = message.content.replace(';', '');
+      const connection = await message.member.voice.channel.join();
 
-    const dispatcher = connection.play(`./audio/${usrMsg}.mp3`);
+      const dispatcher = connection.play(`./audio/${usrMsg}.mp3`);
 
-    dispatcher.on('start', () => {
-      console.log(`${usrMsg} is now playing! 🎷`);
-      message.delete({ timeout: 5000 });
-    });
+      dispatcher.on('start', () => {
+        console.log(`${usrMsg} is now playing! 🎷`);
+        message.delete({ timeout: 5000 });
+      });
 
-    dispatcher.on('finish', () => {
-      console.log(`${usrMsg} has finished playing! 🎬`);
-    });
+      dispatcher.on('finish', () => {
+        console.log(`${usrMsg} has finished playing! 🎬`);
+      });
 
-    dispatcher.on('error', (err) => {
-      console.log(`${usrMsg} error 💣`, err);
-      connection.disconnect();
-    });
-  }
+      dispatcher.on('error', (err) => {
+        console.log(`${usrMsg} error 💣`, err);
+        connection.disconnect();
+      });
 
-  // send a private message with list of sounds available
-  if (message.member.voice.channel && message.content === ';help') {
-    const user = message.author;
-    const list = [];
+    // send a private message with list of sounds available
+    if (message.member.voice.channel && message.content === ';help') {
+      const user = message.author;
+      const list = [];
 
-    soundsList.forEach((sound) => {
-      list.push(sound.replace('.mp3', ''));
-    });
+      soundsList.forEach((sound) => {
+        list.push(sound.replace('.mp3', ''));
+      });
 
-      user.send(list.join('\n;'));
-  }
+        user.send(list.join('\n;'));
+    }
 
-  // randomly play a sound
-  if (message.member.voice.channel && message.content === ';rdm') {
-    // const connection = await message.member.voice.channel.join();
-    
-    // const randomSoundIndex = Math.floor(Math.random() * soundsList.length);
-    // const randomSound = soundsList[randomSoundIndex];
-    const randFile = soundsList[Math.floor(Math.random() * soundsList.length)]
+    // randomly play a sound
+    if (message.member.voice.channel && message.content === ';rdm') {
+      // const connection = await message.member.voice.channel.join();
+      
+      // const randomSoundIndex = Math.floor(Math.random() * soundsList.length);
+      // const randomSound = soundsList[randomSoundIndex];
+      const randFile = soundsList[Math.floor(Math.random() * soundsList.length)]
 
-    const dispatcher = connection.play(`./audio/${randFile}`);
+      const dispatcher = connection.play(`./audio/${randFile}`);
 
-    dispatcher.on('start', () => {
-      console.log(`${randomSound} is randomly playing! 🐙`);
-      message.delete({ timeout: 5000 });
-    });
+      dispatcher.on('start', () => {
+        console.log(`${randomSound} is randomly playing! 🐙`);
+        message.delete({ timeout: 5000 });
+      });
 
-    dispatcher.on('finish', () => {
-      console.log(`${randomSound} has finished playing randomly! 🦑`);
-    });
+      dispatcher.on('finish', () => {
+        console.log(`${randomSound} has finished playing randomly! 🦑`);
+      });
 
-    dispatcher.on('error', (err) => {
-      console.log(`${randomSound} error 💣`, err);
-      connection.disconnect();
-    });
+      dispatcher.on('error', (err) => {
+        console.log(`${randomSound} error 💣`, err);
+        connection.disconnect();
+      });
+    }
   }
 }
